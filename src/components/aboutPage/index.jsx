d1677480534405265aad0bed76a2cf0d1bab3ee4
Rocío <rocioespinosa25@hotmail.com>
2022-03-11 21:11:23 -0300

    11-3

--- src/components/aboutPage/index.jsx
+++ src/components/aboutPage/index.jsx
@@ -6,11 +6,6 @@ export default function index() {
     <div className="about-container">
       <div className="item1">
         <div className="mexico">MEXICO</div>
-        <img
-          className="cafe1"
-          src="https://dam.cocinafacil.com.mx/wp-content/uploads/2019/08/cafe-de-olla.jpg"
-          alt="cafe1"
-        ></img>
       </div>
       <div className="item2">Item 2</div>
       <div className="item3">Item 3</div>
