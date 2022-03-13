7a06db392ee968c40bd5ec42a57c642841e49708
Ivan <ivancheresky@gmail.com>
2022-03-12 22:32:28 -0300

    Set flex items inside container

--- src/components/aboutPage/index.jsx
+++ src/components/aboutPage/index.jsx
@@ -6,6 +6,16 @@ export default function index() {
     <div className="about-container">
       <div className="item1">
         <div className="mexico">MEXICO</div>
+
+        <div className="image-text-container">
+          <img
+            className="mexico-image"
+            src="https://www.littlecoffeeplace.com/wp-content/uploads/2020/07/Mexican-Coffee.jpg"
+            alt="beans"
+          />
+          <span>texto provisorio</span>
+        </div>
+        <span>texto provisorio</span>
       </div>
       <div className="item2">Item 2</div>
       <div className="item3">Item 3</div>
